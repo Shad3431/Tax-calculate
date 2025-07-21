dbc9e27d14cb575117ec975ed928975138274e7b
Shad3431 <96198437+Shad3431@users.noreply.github.com>
2025-07-21 16:14:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -89,6 +89,16 @@ function calculateBelgiumTax(income, rate) {
         totalTax: tax + socialTax + municipalTax
     };
 }
+function updateOptionForCountry(arr) {
+    country.innerHTML = "";
+    arr.map(obj => obj.name)
+        .forEach(s => {
+            const el = document.createElement('option');
+            el.value = s;
+            el.textContent = s;
+            country.appendChild(el);
+        });
+}
 const country = document.getElementById('country');
 updateOptionForCountry(data);
 
