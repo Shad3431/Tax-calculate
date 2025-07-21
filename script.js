9a76dce4fc371908394d56575583808d0729822e
Shad3431 <96198437+Shad3431@users.noreply.github.com>
2025-07-21 15:50:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 import {updateOptionForCountry} from "./src/components/OptionsForCountry.js";
 import {updateOptionForState} from "./src/components/OptionsForState.js";
 import {calculateClick} from "./src/components/CalculateClick.js";
-import { data } from "./src/DB/db.js";
 
+const data = [
+    {
+        name: "Germany",
+        states: [
+            {state: "Bayern", tax: 0.08},
+            {state: "Baden-Württemberg", tax: 0.08},
+            {state: "Berlin", tax: 0.09},
+            {state: "Brandenburg", tax: 0.09},
+            {state: "Bremen", tax: 0.09},
+            {state: "Hamburg", tax: 0.09},
+            {state: "Hessen", tax: 0.09},
+            {state: "Mecklenburg-Vorpommern", tax: 0.09},
+            {state: "Niedersachsen", tax: 0.09},
+            {state: "Nordrhein-Westfalen", tax: 0.09},
+            {state: "Rheinland-Pfalz", tax: 0.09},
+            {state: "Saarland", tax: 0.09},
+            {state: "Sachsen", tax: 0.09},
+            {state: "Sachsen-Anhalt", tax: 0.09},
+            {state: "Schleswig-Holstein", tax: 0.09},
+            {state: "Thüringen", tax: 0.09}
+        ]
+    },
+    {
+        name: "Belgium",
+        states: [
+            {state: "Flanders", tax: 0.07},
+            {state: "Wallonia", tax: 0.08},
+            {state: "Brussels", tax: 0.06},
+        ]
+    },{
+        name: "Ukraine",
+        states: [
+            {state: "Kiev", tax: 0.05}
+        ]
+    }
+]
 const country = document.getElementById('country');
 updateOptionForCountry(data);
 
