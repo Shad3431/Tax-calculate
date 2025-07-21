b8fd77276c667201a0e6e39f7cb96fcaf5d2db17
Shad3431 <96198437+Shad3431@users.noreply.github.com>
2025-07-21 16:12:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,16 @@ function createInfoElement(content, tag) {
     element.append(content);
     return element;
 }
+export function calculateUATax(income) {
+    let tax = 0;
+    if (income <= 86) {
+        tax = income - 31;
+    } else {
+        tax = income * 0.18
+    }
+    return tax
+
+}
 const country = document.getElementById('country');
 updateOptionForCountry(data);
 
